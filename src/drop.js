c34a78b6fa94a32b1150295527e88cb4fbcd32d0
Yoni190 <yfisuhu@gmail.com>
2025-04-21 12:11:35 +0300

    ESLint trial

--- src/drop.js
+++ src/drop.js
@@ -1,12 +1,15 @@
 export class DropDown {
-    constructor(button, elements, visible = false){
+    constructor(button, elements, hideElement=document.querySelector('body'), visible = false){
         this.button = button;
         this.elements = elements;
+        this.hideElement = hideElement;
         this.visible = visible;
     }
 
+
     addFunctionality(){
         this.button.addEventListener('click', ()=>{
+
             if(!this.visible){
                 this.elements.style.display = "block";
                 this.visible = true;
@@ -15,7 +18,7 @@ export class DropDown {
                 this.elements.style.display = "none";
                 this.visible = false;
             }
-            
         })
+        
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import {DropDown} from "./drop.js";
 
 const dropButton = document.querySelector('#drop');
+const body = document.querySelector('body');
 const list = document.querySelector('li');
 
-const dropObject = new DropDown(dropButton, list);
+
+
+const dropObject = new DropDown(dropButton, list, body);
 dropObject.addFunctionality();
 
+
+console.log("Hello")
 
 
 
